efa9e035b8b5b96df17ab9d9ad186389bc7a5838
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-30 00:47:26 +0200

    Realized exercise 23

---
+++ Ex23/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //23.Даний текст.Знайти суму наявних в ньому цифр.
+            string text = "a1b2c3";
+            int sum = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    sum += c - '0';
+                }
+            }
+
+            Console.WriteLine($"Сума всіх цифр: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
