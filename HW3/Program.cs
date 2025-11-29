2e726bcef86081002752fde41c1b5274e6f08ad1
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-29 22:57:19 +0200

    Realized exercise 19

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace HW3
+﻿namespace Ex19
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //19.Знайдіть кількість входжень 'aba' в рядок.
+            string text = "ababaaba";
+            string find = "aba";
+
+            int count = 0;
+
+            for (int i = 0; i <= text.Length - find.Length;)
+            {
+                if (text.Substring(i, find.Length) == find)
+                {
+                    count++;
+                    i += find.Length; 
+                }
+                else
+                {
+                    i++; 
+                }
+            }
+
+            Console.WriteLine($"Кількість входжень 'aba': {count}");
+            Console.ReadKey();
         }
     }
 }
