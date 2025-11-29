b7db87b07a6ca3c6740fb0afa266d903f4066678
dudolad_yaroslav <dudolad2009@gmail.com>
2025-11-29 23:20:59 +0200

    Realized exercise 21

---
+++ Ex21/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //21.Дано рядок, що складається зі слів, розділених символами,
+            //которі перераховані в дргому рядку. Показати всі слова.
+            string text = "привіт,як-справи?";
+
+            string separators = ",-?";
+
+            List<string> words = new List<string>();
+            string current = "";
+
+            foreach (char c in text)
+            {
+                if (separators.Contains(c))
+                {
+                    if (current.Length > 0)
+                    {
+                        words.Add(current);
+                        current = "";
+                    }
+                }
+                else
+                {
+                    current += c;
+                }
+            }
+
+            if (current.Length > 0)
+                words.Add(current);
+
+            foreach (string w in words)
+                Console.WriteLine(w);
+            Console.ReadKey();
+        }
+    }
+}
